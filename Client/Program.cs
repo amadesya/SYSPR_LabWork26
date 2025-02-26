324914d8455a04ed0a106257698eba8ca6ed4116
221 <221@221-005>
2025-02-26 10:07:25 +0300

    Добавьте файлы проекта.

---
+++ Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.IO.Pipes;
+using System.Text;
+
+NamedPipeClientStream client = new(".", "myPipe", PipeDirection.InOut);
+client.Connect();
+
+string message = "Подключение клиента прошло успешно";
+byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+client.Write(messageBytes, 0, messageBytes.Length);
+
+byte[] buffer = new byte[1024];
+int bytesRead = client.Read(buffer, 0, buffer.Length);
+string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+Console.WriteLine(response);
+
+Console.WriteLine("Введите число для факториала");
+string message1 = Console.ReadLine();
+byte[] messageBytes1 = Encoding.UTF8.GetBytes(message1);
+client.Write(messageBytes1, 0, messageBytes1.Length);
+
+byte[] buffer1 = new byte[1024];
+int bytesRead1 = client.Read(buffer1, 0, buffer1.Length);
+string response1 = Encoding.UTF8.GetString(buffer1, 0, bytesRead1);
+Console.WriteLine($"Факториал: {message1}! = {response1}");
+
+
+client.Close();
---
+++ LabWork26/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.IO.Pipes;
+using System.Text;
+
+NamedPipeServerStream server = new("myPipe");
+
+
+server.WaitForConnection(); 
+byte[] buffer = new byte[1024];
+
+int bytesRead = server.Read(buffer, 0, buffer.Length);
+string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+Console.WriteLine($"Message {message}");
+
+string response = "Подключение к серверу произведено";
+byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+server.Write(responseBytes, 0, responseBytes.Length);
+
+int Factorial(int a)
+{
+    if (a == 1) return 1;
+
+     return a * Factorial(a - 1);
+}
+
+
+try
+{
+    byte[] buffer1 = new byte[1024];
+
+    int bytesRead1 = server.Read(buffer1, 0, buffer1.Length);
+    string message1 = Encoding.UTF8.GetString(buffer1, 0, bytesRead1);
+    Console.WriteLine(message1);
+
+    if (int.TryParse(message1, out int a))
+    {
+        int result = Factorial(a);
+        string resultStr = result.ToString();
+        byte[] resultBytes = Encoding.UTF8.GetBytes(resultStr);
+        server.Write(resultBytes, 0, resultBytes.Length);
+        Console.WriteLine($"Факториал:{a}! = {resultStr}");
+    }
+}
+
+catch (IOException e)
+{
+    Console.WriteLine("ERROR: {0}", e.Message);
+}
+
+server.Close();
+
+
+
+
